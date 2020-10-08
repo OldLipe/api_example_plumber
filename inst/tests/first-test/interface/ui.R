44edbf0a2a1226ae1b5d3805f9495997c80fa0bf
Douglas Henrique <barbozadh99@gmail.com>
2020-10-08 17:15:08 -0300

    Add example with httr, requered

--- inst/tests/first-test/interface/ui.R
+++ inst/tests/first-test/interface/ui.R
@@ -4,7 +4,7 @@ library(shinythemes)
 
 shinyUI(fluidPage(
     theme = shinytheme("cerulean"),
-    
+
     navbarPage(
         title = "API - Example",
         id = "nav",
@@ -14,13 +14,13 @@ shinyUI(fluidPage(
                          textInput("Budget", "Enter Value", ""),
                          sliderInput("Year",
                                      "Enter your choice Year",
-                                     min = 1960,
-                                     max = 2020,
-                                     value = 1990)
-                         
-                     ), 
+                                     min = 1960L,
+                                     max = 2020L,
+                                     value = 1990L)
+
+                     ),
                      mainPanel(
-                        textOutput("test"),
+                        plotOutput("test"),
                         textOutput("test1")
                      )
                  )
--- tests/first-test/interface/server.R
+++
@@ -1,9 +0,0 @@
-
-library(shiny)
-
-shinyServer(
-    function(input, output) {
-        output$test <- renderText(input$Budget)
-        output$test1 <- renderText(input$Year)
-    }
-)
