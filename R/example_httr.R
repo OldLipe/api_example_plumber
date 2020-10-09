ac8cc8d442a7a16a5699b3e3bcdedbf6ca9ff45a
Felipe <lipecaso@gmail.com>
2020-10-09 11:36:43 -0300

    update example using httr

--- R/example_httr.R
+++ R/example_httr.R
@@ -1,8 +1,18 @@
+# importing packages
 library(httr)
 library(jsonlite)
-json_mtcars = jsonlite::toJSON(mtcars)
-response <- httr::POST(url = "http://127.0.0.1:9173/clustering/kmeans/groups?centers=3",
-                       body = mtcars, encode = "json")
-mtcars_df <- lapply(httr::content(response), tibble::as_tibble) %>% dplyr::bind_rows()
-## ADD in your server.R (shiny)
-output$test <- renderPlot(plot(mtcars_df$mpg, mtcars_df$cyl, col= mtcars_df$group_id))
+library(magrittr)
+library(dplyr)
+
+# importing mtcars to enviroment
+data("mtcars")
+
+# sending a POST to kmeans API
+res_kmeans <- httr::POST(
+                url = "http://127.0.0.1:9173/clustering/kmeans/groups?centers=3",
+                body = mtcars,
+                encode = "json")
+
+# converting to a tibble data frame
+mtcars_df <- lapply(httr::content(res_kmeans), tibble::as_tibble) %>%
+    dplyr::bind_rows()
