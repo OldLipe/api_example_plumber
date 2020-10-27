50a2fbb9f303c0468c0db48be86a6aca6521c2dd
Felipe <lipecaso@gmail.com>
2020-10-27 17:20:18 -0300

    Add example of using httr

--- R/example_httr.R
+++ R/example_httr.R
@@ -20,13 +20,19 @@ mtcars_df <- lapply(httr::content(res_kmeans), tibble::as_tibble) %>%
 
 ################################################################################
 # sending a POST to som API
+
+# creating url
+url <- httr::modify_url(url   = "http://127.0.0.1:9173/",
+                        path  = "clustering/som/obj",
+                        query = list(xdim  = 5,
+                                     ydim  = 5,
+                                     rlen  = 10,
+                                     alpha = 0.1))
+# making request
 res_som <- httr::POST(
-  url = "http://127.0.0.1:9173/clustering/som/obj?xdim=5&ydim=5",
-  body = mtcars,
+  url    = url,
+  body   = mtcars,
   encode = "json")
 
 # kohonen object from kohonen package
 k_obj <- unserialize(httr::content(res_som, as = "raw"))
-
-
-
